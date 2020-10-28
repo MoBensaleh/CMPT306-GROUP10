aad4baa9879d37cd7c9cc5d8777f4288e42d0487
Marlin Johnston <52979331+MightyMarlin@users.noreply.github.com>
2020-10-28 17:19:44 -0600

    Added Audio and Updated Menus
    Added music, and foot steps, as well as audio mixer and mixer groups. Fixed the look of the menus and added the ability to control different volumes in the options menu.

---
+++ CMPT306 Group 10 Project/Assets/Scripts/AmbientMusic.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class AmbientMusic : MonoBehaviour {
+
+    private AudioSource audioSource;
+
+    void Awake() {
+        DontDestroyOnLoad(transform.gameObject);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic() {
+        if (audioSource.isPlaying) return;
+        audioSource.Play();
+    }
+
+    public void StopMusic() {
+        audioSource.Stop();
+    }
+
+    public void PauseMusic() {
+        audioSource.Pause();
+    }
+
+
+}
+
