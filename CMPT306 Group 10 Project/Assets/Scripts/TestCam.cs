496fcc57dd9d69bc9addbc8b558cac2b875434b3
Evan (Evangellos) Wiegers <evan.wiegers@usask.ca>
2020-10-27 19:02:15 -0600

    Revert "Merge branch 'main' of https://github.com/Danixar/CMPT306-GROUP10 into main"
    This reverts commit 253e5d3b0fd6a55b3272438eb8a25aeb3ba05a47, reversing
    changes made to ea97ff54ea5bf8196445e0f31b6960dd0d9586c2.

---
+++ CMPT306 Group 10 Project/Assets/Scripts/TestCam.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMove : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+    }
+}
