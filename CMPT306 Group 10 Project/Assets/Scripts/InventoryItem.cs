be9fee84580385a6bfd404649a7ba63b0e2be5cb
Mohamed Bensaleh <mobensaleh@outlook.com>
2020-11-02 16:02:59 -0600

    Displays Items
    - Added inventory manager script
    - Added inventory slot scritp

--- CMPT306 Group 10 Project/Assets/Scripts/InventoryItem.cs
+++ CMPT306 Group 10 Project/Assets/Scripts/InventoryItem.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Items")]
 
@@ -12,6 +13,4 @@ public class InventoryItem : ScriptableObject
     public int numberHeld;
     public bool usable;
     public bool unique;
-
-
 }
