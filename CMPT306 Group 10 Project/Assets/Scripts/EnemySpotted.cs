1e01b462f3138ca8c3225debd9f3c6ea21d4b055
Marlin Johnston <52979331+MightyMarlin@users.noreply.github.com>
2020-11-02 16:02:24 -0600

    Worked on enemy spotted. Not working yet.
    Not working yet :(

---
+++ CMPT306 Group 10 Project/Assets/Scripts/EnemySpotted.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemySpotted : MonoBehaviour {
+    private List<GameObject> enemies = new List<GameObject>();
+
+    void OnTriggerEnter(Collider other) {
+        Debug.Log("Trigger!");
+        if (other.tag == "Enemy")
+            enemies.Add(other.gameObject);
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (other.tag == "Enemy")
+            enemies.Remove(other.gameObject);
+    }
+
+    void Update() {
+        RaycastHit hitInfo;
+        foreach (GameObject enemy in enemies) {
+            Ray ray = new Ray(transform.position, transform.position - enemy.transform.position);
+            if (Physics.Raycast(ray, out hitInfo)) {
+                enemy.SendMessage("OnVisible", SendMessageOptions.DontRequireReceiver);
+                Debug.Log("Seen!");
+                enemies.Remove(enemy.gameObject);
+            }
+        }
+    }
+}
