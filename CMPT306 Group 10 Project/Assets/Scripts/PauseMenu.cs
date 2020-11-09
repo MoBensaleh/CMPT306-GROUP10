ba94e25d54a37b791805288c081907977dac091c
Justynpollard1 <jmp447@usask.ca>
2020-11-09 11:59:01 -0600

    Added box and pressure plate mech
    Added pressure plate
    Added pushable box
    Rearranged assets

--- CMPT306 Group 10 Project/Assets/Scripts/PauseMenu.cs
+++ CMPT306 Group 10 Project/Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -47,8 +48,7 @@ public class PauseMenu : MonoBehaviour
 
     public void QuitGame() {
         //Doesn't Work in unity editor
-        Application.Quit();
-        Debug.Log("QUIT");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 
     public void SwitchPanels()
