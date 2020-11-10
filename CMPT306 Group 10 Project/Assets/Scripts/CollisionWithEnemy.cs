34b466f4d10e0f71adf4f8ef8fc33ea047e8b6f4
Marlin Johnston <52979331+MightyMarlin@users.noreply.github.com>
2020-11-10 15:09:32 -0600

    Added transitions between scenes as well as ending sound when caught by enemy

--- CMPT306 Group 10 Project/Assets/Scripts/CollisionWithEnemy.cs
+++ CMPT306 Group 10 Project/Assets/Scripts/CollisionWithEnemy.cs
@@ -5,10 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class CollisionWithEnemy : MonoBehaviour {
 
+    SceneTransition transition;
+    GameOverSound sound;
+
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "Enemy") {
-            Debug.Log("Game over");
-            SceneManager.LoadScene("Gameover");
+            Debug.Log("Enemy hit");
+            Time.timeScale = 0f;
+
+            sound = this.GetComponentInChildren<GameOverSound>();
+            transition = this.GetComponent<SceneTransition>();
+
+            transform.DetachChildren();
+
+            sound.PlayMusic();
+            transition.LoadLevel("Gameover");
         }
     }
 
