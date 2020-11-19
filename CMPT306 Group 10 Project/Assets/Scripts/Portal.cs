5fcccd7c1e9e68a048797011c2862b91c5ad9c96
Justynpollard1 <jmp447@usask.ca>
2020-11-18 18:13:32 -0600

    Added transition from level to level and win

---
+++ CMPT306 Group 10 Project/Assets/Scripts/Portal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    public void goToLevel2() {
+        Time.timeScale = 1f;
+       SceneManager.LoadScene("Game Level 2");
+   }
+
+   public void goToLevel3() {
+        Time.timeScale = 1f;
+       SceneManager.LoadScene("Game Level 3");
+   }
+
+   public void win() {
+       Time.timeScale = 1f;
+       SceneManager.LoadScene("Win");
+   }
+}
