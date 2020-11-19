8165f5cbabeb5b45304598a673f742a3ea41c146
Marlin Johnston <52979331+MightyMarlin@users.noreply.github.com>
2020-11-18 18:48:13 -0600

    Removed win with "v" and changed it to the last portal. Also changed some heirarchy for the sound.

--- CMPT306 Group 10 Project/Assets/Scripts/Portal.cs
+++ CMPT306 Group 10 Project/Assets/Scripts/Portal.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class Portal : MonoBehaviour
 {
+    SceneTransition transition;
+    SoundEffect winSound;
+
     public void goToLevel2() {
         Time.timeScale = 1f;
        SceneManager.LoadScene("Game Level 2");
@@ -16,7 +20,13 @@ public class Portal : MonoBehaviour
    }
 
    public void win() {
-       Time.timeScale = 1f;
-       SceneManager.LoadScene("Win");
-   }
+        Debug.Log("Victory");
+        Time.timeScale = 0f;
+
+        winSound = this.GetComponentInChildren<SoundEffect>();
+        transition = this.GetComponent<SceneTransition>();
+
+        winSound.PlayMusic();
+        transition.LoadLevel("Win");
+    }
 }
