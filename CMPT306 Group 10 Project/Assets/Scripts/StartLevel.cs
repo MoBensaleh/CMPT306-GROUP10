cb0a8fd7f9fa7dc63f888496c141010986406a7d
Evan (Evangellos) Wiegers <evan.wiegers@usask.ca>
2020-11-29 15:35:34 -0600

    Added Mission Intro screen and adjusted inventory amounts for difficulty

---
+++ CMPT306 Group 10 Project/Assets/Scripts/StartLevel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartLevel : MonoBehaviour
+{
+    public GameObject startMenuUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void BeginLevel(){
+        startMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
