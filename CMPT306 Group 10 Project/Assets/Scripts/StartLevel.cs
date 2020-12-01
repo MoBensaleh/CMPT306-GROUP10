6a73f0ef9a1d4ccc02dc4bce3371ccd694387ea6
Mohamed Bensaleh <mobensaleh@outlook.com>
2020-11-30 18:44:19 -0600

    Edited start level script to be off at the start when mission panel is on

--- CMPT306 Group 10 Project/Assets/Scripts/StartLevel.cs
+++ CMPT306 Group 10 Project/Assets/Scripts/StartLevel.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class StartLevel : MonoBehaviour
 {
     public GameObject startMenuUI;
+    public GameObject otherInventoryPanel;
     
     void Start()
     {
+        otherInventoryPanel.SetActive(false);
         startMenuUI.SetActive(true);
         Time.timeScale = 0f;
     }
 
     public void BeginLevel(){
         startMenuUI.SetActive(false);
+        otherInventoryPanel.SetActive(true);
         Time.timeScale = 1f;
     }
 }
