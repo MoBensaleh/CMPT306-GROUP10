fed6f9fe33ee0d699a99ad8ed787b9a500201f24
mackenzielittle <mackenzielittle@protonmail.com>
2020-12-06 11:30:18 -0600

    Got Beads Working
    Beads are collected, counted up on UI, and reset to 0 on player death

---
+++ CMPT306 Group 10 Project/Assets/Scripts/BeadScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BeadScript : MonoBehaviour
+{
+    public TimeCounter timer;
+    private void Update()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name == ("Gameover"))
+        {
+            PlayerBeadPouch.beadTotal = 0;
+            timer.minutes = 0;
+            timer.seconds = 0;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerBeadPouch.beadTotal += 1;
+        BeadUIScript.beadTotal = PlayerBeadPouch.beadTotal;
+        Destroy(gameObject);
+    }
+
+}
---
+++ CMPT306 Group 10 Project/Assets/Scripts/PlayerBeadPouch.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBeadPouch : MonoBehaviour
+{
+    public static int beadTotal = 0;
+}
