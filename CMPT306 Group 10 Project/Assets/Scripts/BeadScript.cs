4883bd86bbe673480ba81798f8151fd018e40376
Mohamed Bensaleh <mobensaleh@outlook.com>
2020-12-07 17:16:31 -0600

    Added mack bead and timer changes

--- CMPT306 Group 10 Project/Assets/Scripts/BeadScript.cs
+++ CMPT306 Group 10 Project/Assets/Scripts/BeadScript.cs
@@ -6,14 +6,34 @@ using UnityEngine.SceneManagement;
 public class BeadScript : MonoBehaviour
 {
     public TimeCounter timer;
+
+    private void Start()
+    {
+
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name == ("Menu"))
+        {
+            PlayerPrefs.DeleteAll();
+        }
+        if (!PlayerPrefs.HasKey("BeadTotal"))
+        {
+            PlayerBeadPouch.beadTotal = 0;
+            PlayerPrefs.SetInt("BeadTotal", PlayerBeadPouch.beadTotal);
+        }
+
+
+    }
+
     private void Update()
     {
         Scene scene = SceneManager.GetActiveScene();
         if (scene.name == ("Gameover"))
         {
             PlayerBeadPouch.beadTotal = 0;
-            timer.minutes = 0;
-            timer.seconds = 0;
+            PlayerPrefs.DeleteKey("prevLevelTime");
+            PlayerPrefs.DeleteKey("BeadTotal");
+            /*            timer.minutes = 0;
+                        timer.seconds = 0;*/
         }
     }
 
@@ -21,6 +41,8 @@ public class BeadScript : MonoBehaviour
     {
         PlayerBeadPouch.beadTotal += 1;
         BeadUIScript.beadTotal = PlayerBeadPouch.beadTotal;
+        PlayerPrefs.SetInt("BeadTotal", PlayerBeadPouch.beadTotal);
+        PlayerPrefs.Save();
         Destroy(gameObject);
     }
 
