2f6ac581aa891d60c4a227875345b6f24916c2cc
mackenzielittle <mackenzielittle@protonmail.com>
2020-12-07 17:05:04 -0600

    Added bead and timer to other levels
    Added bead and timer to other levels, made it persistant through levels, resets on new game.

--- CMPT306 Group 10 Project/Assets/Scripts/TimeCounter.cs
+++ CMPT306 Group 10 Project/Assets/Scripts/TimeCounter.cs
@@ -15,12 +15,24 @@ public class TimeCounter : MonoBehaviour
     void Start()
     {
         counterText = GetComponent<Text>() as Text;
+        if (!PlayerPrefs.HasKey("prevLevelTime"))
+        {
+            PlayerPrefs.SetFloat("prevLevelTime", timer);
+            PlayerPrefs.Save();
+        }
+        else
+        {
+            timer = PlayerPrefs.GetFloat("prevLevelTime");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer = PlayerPrefs.GetFloat("prevLevelTime");
         timer += Time.deltaTime;
+        PlayerPrefs.SetFloat("prevLevelTime", timer);
+        PlayerPrefs.Save();
         minutes = (int)(timer / 60f);
         seconds = (int)(timer % 60f);
         counterText.text = (minutes).ToString("00") + ":" + (seconds).ToString("00");
