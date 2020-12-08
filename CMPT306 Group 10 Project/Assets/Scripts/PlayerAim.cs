5ef54e4a10e02d9a67442d5039ff59d65e313846
Billiam <DanteDevil1005@gmail.com>
2020-12-07 18:09:46 -0600

    pull commit

--- CMPT306 Group 10 Project/Assets/Scripts/PlayerAim.cs
+++ CMPT306 Group 10 Project/Assets/Scripts/PlayerAim.cs
@@ -6,33 +6,93 @@ public class PlayerAim : MonoBehaviour
 {
     Vector3 direction;
     [SerializeField] private FieldOfView fov;
+    private float rotateZ;
+    [SerializeField] private Sprite[] sprites;
+    SpriteRenderer spriteRenderer;
+    public GameObject Player;
+    Transform transform;
+
+    private void Awake()
+    {
+        transform = Player.GetComponent<Transform>();
+        spriteRenderer = Player.GetComponent<SpriteRenderer>();
+    }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         Aim();
+        // Debug.Log(rotateZ);
     }
 
     private void Aim()
     {
+        float fixedZ = -90f;
         direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         direction.Normalize();
 
-        float rotateZ = Mathf.Atan2(-direction.x, direction.y) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, rotateZ + 90);
+        rotateZ = Mathf.Atan2(-direction.x, direction.y) * Mathf.Rad2Deg;
+        //transform.rotation = Quaternion.Euler(0f, 0f, rotateZ + 90);
+        
 
+        spriteRotation();
         fov.SetAimDirection(direction);
 
-  //      Vector3 localScale = Vector3.one;
-  //      if(rotateZ > 90 || rotateZ < -90)
-  //      {
-  //          localScale.y = -1f;
-  //      }
- //       else
- //       {
- //           localScale.y = 1f;
-  //      }
+    }
 
- //       transform.localScale = localScale;
+    private void spriteRotation()
+    {
+        Debug.Log(rotateZ);
+
+        Vector3 localScale = Vector3.one;
+        // Front Facing Left
+        if (rotateZ >= 170f && 180f >= rotateZ)
+        {
+            spriteRenderer.sprite = sprites[0];
+            //Debug.Log("Dir Changed 1");
+        }
+        // More-Left Facing
+        else if (rotateZ >= -105f && -85f >= rotateZ)
+        {
+            spriteRenderer.sprite = sprites[2];
+            //Debug.Log("Dir Changed 2");
+        }
+        // Directly Left Facing
+        else if (rotateZ >= -85f && -55f >= rotateZ)
+        {
+            spriteRenderer.sprite = sprites[3];
+            //Debug.Log("Dir Changed 3");
+        }
+
+        // Back Left 
+        else if (rotateZ <= 5f && -5f >= rotateZ)
+        {
+            spriteRenderer.sprite = sprites[4];
+        }
+        // Back
+        else if (rotateZ <= 90f && 120f >= rotateZ)
+        {
+            spriteRenderer.sprite = sprites[5];
+            localScale.y = -1;
+            transform.localScale = localScale;
+            //Debug.Log("Dir Changed 4");
+        }
+        // Back Right
+        else if (rotateZ <= 15f && 7f >= rotateZ)
+        {
+            spriteRenderer.sprite = sprites[8];
+            localScale.x = -1;
+            transform.localScale = localScale;
+        }
+        // Directly Right
+        else if (rotateZ <= 5f && -7f >= rotateZ)
+        {
+            spriteRenderer.sprite = sprites[7];
+        }
+        // More Right Facing
+        else if (rotateZ <= -165f && -150f >= rotateZ)
+        {
+            spriteRenderer.sprite = sprites[1];
+        }
     }
 }
--- CMPT306 Group 10 Project/Assets/Scripts/PlayerMovement.cs
+++ CMPT306 Group 10 Project/Assets/Scripts/PlayerMovement.cs
@@ -17,16 +17,12 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Vector3 mousePos = viewCamera.
-        // Input
-        //ProcessInputs(); //Uncomment for original, Mac 2020-11-11
-        //transform.LookAt(mouseP)
 
         //Mac 2020-11-11
         moveDirection.x = Input.GetAxisRaw("Horizontal");
         moveDirection.y = Input.GetAxisRaw("Vertical");
 
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition).normalized;
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
 
     }
 
@@ -41,21 +37,6 @@ public class PlayerMovement : MonoBehaviour
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = angle;
 
-        fov.SetOrigin(transform.position);
-    }
-
-    void ProcessInputs()
-    {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
-
-        moveDirection = new Vector2(moveX, moveY).normalized;
-    }
-
-    void Move()
-    {
-      
-        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
         fov.SetOrigin(transform.position);
     }
 }
